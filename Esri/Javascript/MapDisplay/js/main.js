bcac47344c323070307a544e15114fc68a521b47
Sumit Sharma <Sumit.Sharma@cer-rec.gc.ca>
2022-07-21 12:42:32 -0600

    Add widgets

--- Esri/Javascript/MapDisplay/js/main.js
+++ Esri/Javascript/MapDisplay/js/main.js
@@ -1,37 +1,51 @@
-require(["esri/config","esri/Map", "esri/views/MapView", "esri/layers/FeatureLayer","esri/widgets/Legend"], function (esriConfig,Map, MapView,FeatureLayer,Legend) {
+require(["esri/config","esri/Map", "esri/views/MapView", "esri/layers/FeatureLayer","esri/widgets/Legend","esri/widgets/Expand"], function (esriConfig,Map, MapView,FeatureLayer,Legend,Expand) {
 
     esriConfig.apiKey = "YOUR_API_KEY";
 
-const p1= {
-title:"{Name}",
-lastEditInfoEnabled:true,
+const template = {
+title:"{NAME}",
+lastEditInfoEnabled:false,
 content: [
   {
-  fieldname:"",
-  label:""
-},
-  {
-    fieldname:"",
-    label:""
-  }
-]
+    type:"fields",
+    fieldInfos:
+    [
+      {
+      fieldname:"POP2007",
+      label:"Population in 2007"
+    }
+  ]  
+}]
 };
+
 const f1= new FeatureLayer(
   {
-    url:"https://sampleserver6.arcgisonline.com/arcgis/rest/services/USA/MapServer/3"
+    url:"https://sampleserver6.arcgisonline.com/arcgis/rest/services/Census/MapServer/2",
+    popupTemplate: template
   }
 );
     const map = new Map({
       basemap: "arcgis-topographic", // Basemap layer service
-      layers:[f1],
-      PopupTemplate:p1
+      layers:[f1]
     });
 
     const view = new MapView({
       map: map,
       center: [-118.805, 34.027], // Longitude, latitude
-      zoom: 13, // Zoom level
+      zoom:2, // Zoom level
       container: "viewDiv" // Div element
     });
-
+const legend = new Legend(
+  {
+  view:view,
+  container:"legendDiv"
+  }
+);
+const expand = new Expand(
+  {
+    view:view,
+    content:legend
+  }
+);
+view.ui.add(expand,"top-right");
   });
