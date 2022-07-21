400fa7f86da7d6c9f2acd57401577de104a94621
Sumit Sharma <Sumit.Sharma@cer-rec.gc.ca>
2022-07-20 18:18:59 -0600

    Added Feature layer

--- Esri/Javascript/MapDisplay/js/main.js
+++ Esri/Javascript/MapDisplay/js/main.js
@@ -1,9 +1,14 @@
-require(["esri/config","esri/Map", "esri/views/MapView"], function (esriConfig,Map, MapView) {
+require(["esri/config","esri/Map", "esri/views/MapView", "esri/layers/FeatureLayer"], function (esriConfig,Map, MapView,FeatureLayer) {
 
     esriConfig.apiKey = "YOUR_API_KEY";
-
+const f1= new FeatureLayer(
+  {
+    url:"https://sampleserver6.arcgisonline.com/arcgis/rest/services/USA/MapServer/3"
+  }
+);
     const map = new Map({
-      basemap: "arcgis-topographic" // Basemap layer service
+      basemap: "arcgis-topographic", // Basemap layer service
+      layers:[f1]
     });
 
     const view = new MapView({
