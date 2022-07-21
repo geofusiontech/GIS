27c8ca9136b869f0de42528a88d906dbe512166b
Sumit Sharma <Sumit.Sharma@cer-rec.gc.ca>
2022-07-21 11:54:14 -0600

    popup

--- Esri/Javascript/MapDisplay/js/main.js
+++ Esri/Javascript/MapDisplay/js/main.js
@@ -1,6 +1,21 @@
-require(["esri/config","esri/Map", "esri/views/MapView", "esri/layers/FeatureLayer"], function (esriConfig,Map, MapView,FeatureLayer) {
+require(["esri/config","esri/Map", "esri/views/MapView", "esri/layers/FeatureLayer","esri/widgets/Legend"], function (esriConfig,Map, MapView,FeatureLayer,Legend) {
 
     esriConfig.apiKey = "YOUR_API_KEY";
+
+const p1= {
+title:"{Name}",
+lastEditInfoEnabled:true,
+content: [
+  {
+  fieldname:"",
+  label:""
+},
+  {
+    fieldname:"",
+    label:""
+  }
+]
+};
 const f1= new FeatureLayer(
   {
     url:"https://sampleserver6.arcgisonline.com/arcgis/rest/services/USA/MapServer/3"
@@ -8,7 +23,8 @@ const f1= new FeatureLayer(
 );
     const map = new Map({
       basemap: "arcgis-topographic", // Basemap layer service
-      layers:[f1]
+      layers:[f1],
+      PopupTemplate:p1
     });
 
     const view = new MapView({
